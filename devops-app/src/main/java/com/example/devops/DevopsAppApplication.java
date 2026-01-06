af9a48c417ea18b1b4fefa23e8544d850f558eff
Omar <Omar.amakhlouf@gmail.com>
2026-01-06 23:23:00 +0100

    Application Java Maven affichant un message de motivation

---
+++ devops-app/src/main/java/com/example/devops/DevopsAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.devops;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+	}
+
+}
